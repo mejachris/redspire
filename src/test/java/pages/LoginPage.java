b29848239be785a3690c4c38783edb49e8802e61
ChristopherMeja <ChristopherMeja@RedspireSP017.home>
2020-12-15 11:15:09 +0000

    changes

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,10 +1,10 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import testBase.BaseClass;
 
 public class LoginPage extends BaseClass{
@@ -14,19 +14,31 @@ public class LoginPage extends BaseClass{
     public WebElement userEmail;
     @FindBy(xpath = "//input[@id = 'user_password']")
     public WebElement userPassword;
-   
- 
+    @FindBy (xpath ="//INPUT[@type='submit']")
+    public WebElement Login;
+    
+       
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void userEmail(String name) {
     	userEmail.sendKeys(name);
-    	    	
     }
-    
+     
     public void userPassword(String userpassword) {
     	userPassword.sendKeys(userpassword); 	    	
     }
+    
+    public void login() {
+    	Login.click();
+    }
+    
 
 }
+
+   
+    	
+    	
+    
+
